47a159bda073d9acc1ce884be65551625f9fae66
Lal Saud <lalusaud@gmail.com>
2021-01-07 23:32:12 -0500

    :+1: Use default options

--- lib/makuri/browser_builder/base.rb
+++ lib/makuri/browser_builder/base.rb
@@ -2,10 +2,10 @@ module Makuri::BrowserBuilder
   class Base
     attr_accessor :user_agent, :request_method, :request_body
 
-    def initialize(options)
-      @user_agent     = options.fetch(:user_agent)
-      @request_method = options.fetch(:request_method)
-      @request_body   = options.fetch(:request_body)
+    def initialize(options = {})
+      @user_agent     = options.fetch(:user_agent, '')
+      @request_method = options.fetch(:request_method, :get)
+      @request_body   = options.fetch(:request_body, '')
     end
   end
 end
