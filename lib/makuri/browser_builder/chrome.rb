e46b0be8ecabcd5e5c0f170bf755b479378df548
Lal Saud <lalusaud@gmail.com>
2021-02-05 18:19:54 -0500

    :+1: Add ability to pass :enable_images to chrome

--- lib/makuri/browser_builder/chrome.rb
+++ lib/makuri/browser_builder/chrome.rb
@@ -3,9 +3,12 @@ require 'selenium-webdriver'
 
 module Makuri::BrowserBuilder
   class Chrome < Base
+    attr_accessor :headless, :enable_images
+
     def initialize(options = {})
       super
-      @headless = options.fetch(:headless, true)
+      @headless      = options.fetch(:headless, true)
+      @enable_images = options.fetch(:enable_images, true)
     end
 
     def build
@@ -23,11 +26,11 @@ module Makuri::BrowserBuilder
         --disable-gpu
         --no-sandbox
         --disable-translate
-        --blink-settings=imagesEnabled=false
         --ignore-certificate-errors
       ]
-      args << '--headless' if @headless
-      args << "--user-agent=#{@user_agent}"
+      args << '--headless' if headless
+      args << "--user-agent=#{user_agent}"
+      args << "--blink-settings=imagesEnabled=#{enable_images}"
       Selenium::WebDriver::Chrome::Options.new(args: args)
     end
   end
